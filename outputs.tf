24517154e37ed63bf968b88c6864d3174a4b902e
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-11 00:07:51 -0700

    adding dns outputs

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,23 @@ output "apps_Alpha" {
 output "apps_Bravo" {
   value = "${module.app_Bravo.ip_addresses}"
 }
+
+output "proxy_host" {
+  value = "${module.proxy.dns}"
+}
+
+output "alpha_dns" {
+  value = "${module.app_Alpha.dns}"
+}
+
+output "alpha_dns_nodes" {
+  value = "${module.app_Alpha.dns_nodes}"
+}
+
+output "beta_dns" {
+  value = "${module.app_Beta.dns}"
+}
+
+output "beta_dns_nodes" {
+  value = "${module.app_Beta.dns_nodes}"
+}
